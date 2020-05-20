b0767fef1d9ebe905a654d1be5ac442e35e7cc7a
experimentsincs <experimentsincs@gmail.com>
2020-05-20 16:43:57 -0400

    Modified imported kart with wheel colliders and organization

--- KartRacer/Assets/AutoRotator.cs
+++ KartRacer/Assets/AutoRotator.cs
@@ -7,7 +7,7 @@ public class AutoRotator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start() called");
     }
 
     // Update is called once per frame
